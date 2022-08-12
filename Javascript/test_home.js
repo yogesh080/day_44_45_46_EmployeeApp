25d2c9cfd30e4530fd6ccf8d4d884f56478a4f36
Yogesh <yogeshbietjhs@gmail.com>
2022-08-12 22:14:03 +0530

    dynamic department added

--- Javascript/test_home.js
+++ Javascript/test_home.js
@@ -7,20 +7,20 @@ createInnerHtml = () =>{
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                         "<th>Salary</th><th>Start Date</th><th>Actions</th>"
 
-    let empPayrollData = createEmplooyeePayrollJSON()[0];
+    let empPayrollData = createEmplooyeePayrollJSON()[2];
     const innerHtml = `${headerHtml}
+    
     <tr>
                 
               </tr>
               <tr>
-                <td><img class="profile" alt="" src="./asserts/elon.jpg"></td>
-                <td>Yogesh</td>
-                <td>Male</td>
-                <td><div class='dept-label'>HR</div>
-                    <div class="dept-label">Finance</div>
-                </td>
-                <td>500000</td>
-                <td>18 September 2020</td>
+                <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+                <td>${empPayrollData._name}</td>
+                <td>${empPayrollData._gender}</td>
+                <td>${getDeptHtml(empPayrollData._department)}</td>
+
+                <td>${empPayrollData._salary}</td>
+                <td>${empPayrollData._startDate}</td>
                 <td><img id="1" onclick= "remove(this)" alt="delete" src="./asserts/icons8-delete-30.png">
                   <img id="1" onclick= "update(this)" alt="edit" src="./asserts/icons8-edit-30.png"></td>
               </tr>
@@ -44,19 +44,41 @@ const createEmplooyeePayrollJSON = () =>{
             _profilePic:'./asserts/elon.jpg'
         },
         {
-            _name : 'Yogesh yadav',
+            _name : 'Raj shekhar',
             _gender:'male',
             _department:[
                 'Engineering',
-                'Finance'
+                'HR'
             ],
             _salary:'500000',
             _startDate: '29 Oct 2019',
             _note:'',
             _id: new Date().getTime(),
-            _profilePic:'./asserts/elon.jpg'
+            _profilePic:'./asserts/jeff.jpg'
 
+        },
+        {
+            _name : 'Mary Barra',
+            _gender:'Female',
+            _department:[
+                'Engineering',
+                'Finance',
+                'HR'
+            ],
+            _salary:'800000',
+            _startDate: '29 Oct 2020',
+            _note:'',
+            _id: new Date().getTime(),
+            _profilePic:'./asserts/mary.jpg'
         }
     ];
     return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList){
+        deptHtml = `${deptHtml} <div class ='dept-label'>${dept}</div>`
+    }
+    return deptHtml
 }
